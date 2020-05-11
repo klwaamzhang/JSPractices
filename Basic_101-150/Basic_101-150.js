3bda5e5ff8b4b6caffd36afab3001c50e4e7c9e8
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-05-11 19:45:37 -0400

    Q103-105

--- Basic_101-150/Basic_101-150.js
+++ Basic_101-150/Basic_101-150.js
@@ -1,18 +1,66 @@
+// A105
+document.getElementById("btnFindQ105").addEventListener("click", () => {
+  let numQ105 = document.getElementById("strQ105").value;
+  let count = 0;
+  let timesFuncQ105 = (num) => {
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+      sum += parseInt(num[i]);
+      console.log(sum);
+    }
+    count++;
+    if (sum < 10) {
+      return count;
+    } else {
+      let num = sum.toString();
+      timesFuncQ105(num);
+    }
+  };
+  timesFuncQ105(numQ105);
+  document.getElementById("txtAnsQ105").innerHTML = count;
+});
 
+// A104
+document.getElementById("btnFindQ104").addEventListener("click", () => {
+  let arrQ104 = document.getElementById("strQ104").value.split(" ");
+  let iQ104 = parseInt(document.getElementById("iQ104").value);
+  let resultQ104 = [
+    arrQ104[0],
+    arrQ104[1],
+    Math.abs(parseInt(arrQ104[1]) - parseInt(arrQ104[0])),
+  ];
+  for (let i = 0; i < arrQ104.length - 1; i++) {
+    for (let j = i + 1; j < arrQ104.length; j++) {
+      let diff = Math.abs(parseInt(arrQ104[j]) - parseInt(arrQ104[i]));
+      if (diff <= iQ104 && diff >= resultQ104[2]) {
+        resultQ104 = [arrQ104[i], arrQ104[j], diff];
+      }
+    }
+  }
+  document.getElementById("txtAnsQ104").innerHTML = resultQ104;
+});
+
+// A103
+document.getElementById("btnCalculateQ103").addEventListener("click", () => {
+  let strQ103 = document.getElementById("strQ103").value;
+  let indexD = parseInt(document.getElementById("iQ103").value);
+  let maxNum = parseInt(strQ103.slice(0, indexD) + strQ103.slice(indexD + 1));
+  document.getElementById("txtAnsQ103").innerHTML = maxNum;
+});
 
 // A102
 document.getElementById("btnFindQ102").addEventListener("click", () => {
-    let arrQ102 = document.getElementById("strQ102").value.split(" ");
-    let count = 0;
-    for (let i = 0; i < arrQ102.length - 1; i++) {
-      for (let n = i + 1; n < arrQ102.length; n++) {
-        if (arrQ102[i] > arrQ102[n]) {
-          count++;
-        }
+  let arrQ102 = document.getElementById("strQ102").value.split(" ");
+  let count = 0;
+  for (let i = 0; i < arrQ102.length - 1; i++) {
+    for (let n = i + 1; n < arrQ102.length; n++) {
+      if (arrQ102[i] > arrQ102[n]) {
+        count++;
       }
     }
-    document.getElementById("txtAnsQ102").innerHTML = count;
-  });
+  }
+  document.getElementById("txtAnsQ102").innerHTML = count;
+});
 
 // A101
 document.getElementById("btnCheckQ101").addEventListener("click", () => {
