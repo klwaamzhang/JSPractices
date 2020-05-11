13e8176b468ebc3260159e730eb7476e97d27901
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-05-11 14:52:51 -0400

    Q102

--- Basic_101-150/Basic_101-150.js
+++ Basic_101-150/Basic_101-150.js
@@ -1,3 +1,19 @@
+
+
+// A102
+document.getElementById("btnFindQ102").addEventListener("click", () => {
+    let arrQ102 = document.getElementById("strQ102").value.split(" ");
+    let count = 0;
+    for (let i = 0; i < arrQ102.length - 1; i++) {
+      for (let n = i + 1; n < arrQ102.length; n++) {
+        if (arrQ102[i] > arrQ102[n]) {
+          count++;
+        }
+      }
+    }
+    document.getElementById("txtAnsQ102").innerHTML = count;
+  });
+
 // A101
 document.getElementById("btnCheckQ101").addEventListener("click", () => {
   let strQ101 = document.getElementById("strQ101").value;
